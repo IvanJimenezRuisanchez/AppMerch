3d2c15214638daf26160d49e55285c6437512b65
ivanjr <ivanjr1802@gmail.com>
2022-09-15 22:53:58 -0400

    Component pannier

---
+++ ecommerce_frontend/src/components/Products/Pannier.vue
@@ -0,0 +1,17 @@
+<template>
+        <div class="card">
+          <div class="card-body">
+            
+            <button type="button" class="btn btn-warning btn-block btn-lg">Proceed to Pay</button>
+          </div>
+        </div>
+</template>
+
+<script>
+export default{
+    name: 'Pannier',
+}
+</script>
+
+<style>
+</style>
---
+++ src/main/resources/dta.sql
@@ -0,0 +1,14 @@
+INSERT INTO  PRODUIT  (id,prix,nom,categorie) VALUES (1,12.30,'Jordan','hoodie');
+INSERT INTO  PRODUIT  (id,prix,nom,categorie) VALUES (2,32,'Jordan','hoodie');
+INSERT INTO  PRODUIT  (id,prix,nom,categorie) VALUES (3,45,'Nike','chandail');
+INSERT INTO  PRODUIT  (id,prix,nom,categorie) VALUES (4,67,'Jordan','hoodie');
+INSERT INTO  PRODUIT  (id,prix,nom,categorie) VALUES (5,1000,'Jordan','hoodie');
+INSERT INTO  PRODUIT  (id,prix,nom,categorie) VALUES (6,23,'Jordan','hoodie');
+INSERT INTO  PRODUIT  (id,prix,nom,categorie) VALUES (7,6,'Jordan','hoodie');
+INSERT INTO  PRODUIT  (id,prix,nom,categorie) VALUES (8,30,'Jordan','hoodie');
+INSERT INTO  PRODUIT  (id,prix,nom,categorie) VALUES (9,50,'Nike','chandail');
+INSERT INTO  PRODUIT  (id,prix,nom,categorie) VALUES (10,400,'Nike','chandail');
+INSERT INTO  PRODUIT  (id,prix,nom,categorie) VALUES (12,600,'Nike','chandail');
+INSERT INTO  PRODUIT  (id,prix,nom,categorie) VALUES (13,600,'Nike','chandail');
+INSERT INTO  PRODUIT  (id,prix,nom,categorie) VALUES (14,600,'Nike','chandail');
+
